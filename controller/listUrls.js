ec2a51f98ac8faa238e5bc4d3b0d84be2912e97b
Dario D. Müller <dario-domi@hotmail.de>
2017-12-29 04:12:02 +0100

    ADDED listUrls WIP

--- controller/listUrls.js
+++ controller/listUrls.js
@@ -1,3 +1,6 @@
+var db = require('./../db')
+
+var linksDB = db.linksDB.instance
 
 module.exports = {
 	
@@ -5,6 +8,33 @@ module.exports = {
 		// TODO READ DB and show a list of URLs here
 		console.log('/listUrls GET')
 		res.send('/listUrls GET')
+
+		var throw200 = function(data) {
+			//res.writeHead(200)
+			if(typeof(data) === 'object')
+				data = JSON.stringify(data);
+			res.end(data)
+			console.log(data)
+		}
+		var throw404 = function(err) {
+			if(err !== null)
+				console.log(err)
+			res.writeHead(404)
+			res.end()
+		}
+
+		var findSuccessful = function(data) {
+			throw200(data)
+		}
+		var findFailed  = function(err) {
+			throw404(err)
+		}
+		var conditions = {}
+		linksDB
+			.find(conditions)
+			.then(findSuccessful)
+			.catch(findFailed)
+
 	}
 	
 }
